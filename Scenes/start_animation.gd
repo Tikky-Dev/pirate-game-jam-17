1330af5b68f9a288af5eb85fb3d60881edbc03aa
BennWHH <bdedominicas@gmail.com>
2025-07-17 15:42:34 -0400

    changed a little bit with the player
    I think I changed the movement to make it a little smoother and scaled the player up in the world

--- Scenes/start_animation.gd
+++
@@ -1,6 +0,0 @@
-extends VideoStreamPlayer
-
-
-
-func _on_finished() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Menu.tscn") # Replace with function body.
--- Scripts/start_animation.gd
+++ Scripts/start_animation.gd
@@ -1,5 +1,6 @@
 extends VideoStreamPlayer
 
 
+
 func _on_finished() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Menu.tscn") # Replace with function body.
