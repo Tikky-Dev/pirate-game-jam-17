4ad3ecd0a2ffe577430d4883d710d340b553dc1b
Noah-Tumbaga <ivs.bandit@gmail.com>
2025-07-24 11:01:39 -0700

    Fixed bug in arm_pivot

--- Scripts/arm_pivot.gd
+++ Scripts/arm_pivot.gd
@@ -9,11 +9,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if $"..".scale.x==1:
-		if position.angle_to_point(get_global_mouse_position())< 3.14*0.30 && position.angle_to_point(get_global_mouse_position())>-3.14*0.30:
+		if global_position.angle_to_point(get_global_mouse_position())< 3.14*0.30 && position.angle_to_point(get_global_mouse_position())>-3.14*0.30:
 			look_at(get_global_mouse_position())
 	else:
-		if position.angle_to_point(get_global_mouse_position())> 3.14*0.8 || position.angle_to_point(get_global_mouse_position())< -0.8*3.14:
+		if global_position.angle_to_point(get_global_mouse_position())> 3.14*0.8 || position.angle_to_point(get_global_mouse_position())< -0.8*3.14:
 			look_at(get_global_mouse_position())
-		print(position.angle_to_point(get_global_mouse_position())/3.1415926535)
+		
 	rotation_degrees 
 	pass
