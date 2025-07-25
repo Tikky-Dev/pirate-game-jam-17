05edcbd7d615780bb845560743ea07be5d5ebf66
Noah-Tumbaga <ivs.bandit@gmail.com>
2025-07-25 14:38:01 -0700

    simple sound on door

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 var is_open = false
 var can_interact = false
+@onready var door_sound_player: AudioStreamPlayer2D = $DoorSoundPlayer
 
 func _input(event: InputEvent) -> void:
 	if can_interact:
@@ -12,11 +13,16 @@ func _input(event: InputEvent) -> void:
 				$StaticBody2D/ToggleCollision.set_deferred("disabled", false)
 				is_open = false
 				$DoorOccluder.visible=true
+				door_sound_player.pitch_scale=randf_range(0.3,0.5)
+				door_sound_player.play(2.3)
 			elif not is_open:
 				while ($Top.position != Vector2(0, -42)):
 					$Top.position.y -= 1
 				$StaticBody2D/ToggleCollision.set_deferred("disabled", true)
 				$DoorOccluder.visible=false
+				door_sound_player.pitch_scale=randf_range(0.5,0.7)
+				door_sound_player.play(2.3)
+				
 				is_open = true
 
 func _on_body_entered(body: Node2D) -> void:
